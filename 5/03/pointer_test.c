df4bf9a018c95cb8b4cc2ad9e6e2927a16d8be58
Yuya Yabe <yuyabee@gmail.com>
2014-09-21 16:00:57 -0700

    wip: change coding style

--- 5/03/pointer_test.c
+++ 5/03/pointer_test.c
@@ -2,11 +2,11 @@
 
 int main()
 {
-	int a[10] = {1,2,3,4,5,6,7};
-	int *pa = &a[0];
-	int x = *(pa+1);
+  int a[10] = {1,2,3,4,5,6,7};
+  int *pa = &a[0];
+  int x = *(pa+1);
 
-	printf("%d\n", x);
+  printf("%d\n", x);
 
-	return 0;
+  return 0;
 }
