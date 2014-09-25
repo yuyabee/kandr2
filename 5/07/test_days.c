4d6a4abfd12974537afc0067801d85cc5ed41be8
Yuya Yabe <yuyabee@gmail.com>
2014-09-24 20:48:09 -0700

    add test to test month_day()

--- 5/07/test_days.c
+++ 5/07/test_days.c
@@ -6,6 +6,10 @@ void month_day(int, int, int*, int*);
 
 int main()
 {
+  int m, d;
   printf("%d\n", day_of_year(2014, 9, 24));
+  month_day(2014, 60, &m, &d);
+  printf("%d/%d\n", m, d);
+
   return 0;
 }
