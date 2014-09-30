ef49914cf3366ad65255304ae58371f1ae8b8c55
Yuya Yabe <yuyabee@gmail.com>
2014-09-29 22:54:50 -0700

    find with arguments

--- 5/10/find.c
+++ 5/10/find.c
@@ -7,16 +7,37 @@ int getLine(char *line, int max);
 int main(int argc, char *argv[])
 {
   char line[MAXLINE];
-  int found = 0;
+  long lineno = 0;
+  int c, except = 0, number = 0, found = 0;
 
-  if (argc != 2)
-    printf("Usage: find pattern\n");
+  while (--argc && **++argv == '-')
+    while (c = *++argv[0])
+      switch (c) {
+        case 'x':
+          except = 1;
+          break;
+        case 'n':
+          number = 1;
+          break;
+        default:
+          printf("find: illegal option %c\n", c);
+          argc = 0;
+          found = -1;
+          break;
+      }
+
+  if (argc != 1)
+    printf("Usage: find -x -n pattern\n");
   else
-    while (getLine(line, MAXLINE) > 0)
-      if (strstr(line, argv[1]) != NULL) {
+    while (getLine(line, MAXLINE) > 0) {
+      lineno++;
+      if ((strstr(line, *argv) != NULL) != except) {
+        if (number)
+          printf("%ld", lineno);
         printf("%s", line);
         found++;
       }
+    }
 
   return found;
 }
