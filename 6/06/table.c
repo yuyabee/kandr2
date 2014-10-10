6c9545093a29f9b0ea7b37b25e2d5ece4a991a90
Yuya Yabe <yuyabee@gmail.com>
2014-10-10 16:30:00 -0700

    WIP 6-5

--- 6/06/table.c
+++ 6/06/table.c
@@ -14,6 +14,7 @@ struct nlist *hashtab[HASHSIZE];
 
 unsigned hash(char *);
 struct nlist *lookup(char *);
+char *lookupdefn(char *s);
 struct nlist *install(char *, char *);
 void undef(char *);
 
@@ -25,15 +26,14 @@ int main()
   for (int i = 0; i < 3; i++)
     printf("Next exists if it is 1: %d\n", (install(testNames[i], testDefns[i])->next == NULL) ? 0 : 1);
 
-  struct nlist *np1 = lookup("test1");
-  printf("Name: %s, Defn: %s\n", np1->name, np1->defn);
+  printf("Name: %s, Defn: %s\n", testNames[0], lookupdefn("test1"));
 
   struct nlist *np3 = lookup("test3");
-  printf("Name: %s, Defn: %s\n", np3->name, np3->defn);
+  printf("Name: %s, Defn: %s\n", testNames[2], lookupdefn("test3"));
 
   undef("test2");
 
-  printf("test2 should not exist 0: %d\n", (lookup("test2") == NULL) ? 0 : 1);
+  printf("test2 should not exist 0: %s\n", lookupdefn("test2"));
   return 0;
 }
 
@@ -53,9 +53,21 @@ lookup(char *s)
   struct nlist *np;
 
   for (np = hashtab[hash(s)]; np != NULL; np = np->next)
-    if ((strcmp(s, np->name)) == 0)
+    if (strcmp(s, np->name) == 0) {
+      printf("s: %s\n", s);
       return np;
+    }
   return NULL;
+}
+
+char *
+lookupdefn(char *s)
+{
+  struct nlist *np = lookup(s);
+  if (np != NULL)
+    return np->defn;
+  else
+    return "not defined";
 }
 
 struct nlist *
