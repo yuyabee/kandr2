72ab4dcf9ebf57e94e960311d093400926e74b09
Yuya Yabe <yuyabee@gmail.com>
2014-10-15 11:38:44 -0700

    add cat.c

---
+++ 7/05/cat.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void filecopy(FILE *, FILE *);
+int main(int argc, char *argv[])
+{
+  FILE *fp;
+  if (argc == 1)
+    filecopy(stdin, stdout);
+  else {
+    if ((fp = fopen(*++argv, "r")) == NULL)
+      printf("cat: couldn't open the file %s\n", *argv);
+    else {
+      filecopy(fp, stdout);
+      fclose(fp);
+    }
+  }
+  return 0;
+}
+
+void filecopy(FILE *ifp, FILE *ofp)
+{
+  int c;
+  while ((c = getc(ifp)) != EOF)
+    putc(c, ofp);
+}
