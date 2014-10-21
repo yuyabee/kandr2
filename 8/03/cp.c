6b716329874d849b6cabd86f128e80315e23c946
Yuya Yabe <yuyabee@gmail.com>
2014-10-21 11:30:12 -0700

    add cp.c

---
+++ 8/03/cp.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include "unistd.h"
+#include <stdlib.h>
+
+// allow read and write to every users
+#define PERMS 0666
+#define BUFSIZ 1024
+
+int main(int argc, char *argv[])
+{
+  int f1, f2;
+  int n;
+  char buff[BUFSIZ];
+
+  if (argc != 3) {
+    printf("cp: Usage cp from to\n");
+    exit(1);
+  }
+  if ((f1 = open(argv[1], O_RDONLY, PERMS)) == -1) {
+    printf("cp: could not open %s\n", argv[0]);
+    exit(1);
+  }
+  if ((f2 = creat(argv[2], PERMS)) == -1) {
+    printf("cp: could not create %s with permission %d\n", argv[2], PERMS);
+  }
+
+  if ((n = read(f1, buff, BUFSIZ)) > 0)
+    write(f2, buff, n);
+
+  return 0;
+}
