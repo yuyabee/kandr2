2ea5f5fb6f7a1ec06784d0e2fc00642afca2ae6b
Yuya Yabe <yuyabee@gmail.com>
2014-10-31 11:06:32 -0700

    WIP change file name

--- 8/05/fopen.c
+++ 8/05/fopen.c
@@ -1,5 +1,4 @@
-#include <stdlib.h>
-#include "syscalls.c"
+#include "futils.c"
 
 #define PERMS 0666
 
--- 8/05/futils.c
+++ 8/05/futils.c
@@ -2,8 +2,7 @@
 #include <fcntl.h>
 #include "unistd.h"
 
-#ifdef NULL
-#elif
+#ifndef NULL
   #define NULL      0
 #endif
 #define EOF       (-1)
@@ -17,6 +16,10 @@ typedef struct _iobuf {
   int   flag;   /* mode of file access */
   int   fd;     /* file descriptor */
 } FILE;
+
+FILE _iob[OPEN_MAX] = {
+};
+
 extern FILE _iob[OPEN_MAX];
 
 #define stdin   (&_iob[0])
@@ -67,37 +70,4 @@ int _fillbuf(FILE *fp)
     return EOF;
   }
   return (unsigned char) *fp->ptr++;
-}
-
-#define PERMS 0666
-
-FILE *fopen(char *name, char *mode)
-{
-  int   fd;
-  FILE  *fp;
-  FILE _iob[OPEN_MAX];
-
-  if (*mode != 'r' && *mode != 'w' && *mode != 'a')
-    return NULL;
-  for (fp = _iob; fp < _iob + OPEN_MAX; fp++)
-    if ((fp->flag & (_READ | _WRITE)) == 0)
-      break;
-  if (fp >= _iob + OPEN_MAX)
-    return NULL;
-
-  if (*mode == 'w')
-    fd = creat(name, PERMS);
-  else if (*mode == 'a') {
-    if ((fd = open(name, O_WRONLY, 0)) == -1)
-      fd = creat(name, PERMS);
-    lseek(fd, 0L, 2);
-  } else
-    fd = open(name, O_RDONLY, 0);
-  if (fd == -1)
-    return NULL;
-  fp->fd    = fd;
-  fp->cnt   = 0;
-  fp->base  = NULL;
-  fp->flag  = (*mode == 'r') ? _READ : _WRITE;
-  return fp;
 }
