77bb76d76500b72a33748122fa503e9057353dd9
Yuya Yabe <yuyabee@gmail.com>
2014-11-03 10:22:30 -0800

    add fsize.c

--- 8/06/fsize.c
+++ 8/06/fsize.c
@@ -7,6 +7,7 @@
 
 int stat(char *, struct stat *);
 void dirwalk(char *, void (*fcn)(char *));
+
 void fsize(char *);
 
 int main(int argc, char **argv)
@@ -17,4 +18,43 @@ int main(int argc, char **argv)
     while (--argc > 0)
       fsize(*++argv);
   return 0;
+}
+
+void fsize(char *name)
+{
+  struct stat stbuf;
+
+  if (stat(name, &stbuf) == -1) {
+    fprintf(stderr, "fsize: can't access %s\n", name);
+    return;
+  }
+  if ((stbuf.st_mode & S_IFMT) == S_IFDIR)
+    dirwalk(name, fsize);
+  printf("%8ld %s\n", stbuf.st_size, name);
+}
+
+#define MAX_PATH 1024
+
+void dirwalk(char *dir, void (*fcn)(char *))
+{
+  char name[MAX_PATH];
+  Dirent *dp;
+  DIR *dfd;
+
+  if ((dfd = opendir(dir)) == NULL) {
+    fprintf(stderr, "dirwalk: can't open %s\n", dir);
+    return;
+  }
+
+  while ((dp = readdir(dfd)) != NULL) {
+    if (strcmp(dp->name, ".") == 0 || strcmp(dp->name, "..") == 0)
+      continue;
+    if (strlen(dir) + strlen(dp->name) + 2 > sizeof(name))
+      fprintf(stderr, "dirwalk: name %s/%s is too long\n", dir, dp->name);
+    else {
+      sprintf(name, "%s/%s", dir, dp->name);
+      (*fcn)(name);
+    }
+  }
+  closedir(dfd);
 }
